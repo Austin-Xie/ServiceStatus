b5552936c449255542e0297fad0f47912ce42899
Austin Xie <Austin.Xie@optus.com.au>
2013-07-21 14:30:24 +1000

    rename network_outage.php to list.php to follow RightNow's convention

--- development/views/pages/service/status/list.php
+++ development/views/pages/service/status/list.php
@@ -1,22 +1,5 @@
 <rn:meta title="#rn:msg:SHP_TITLE_HDG#" template="service_status_query.php" clickstream="home"/>
 
-
-
-<!--
-<div id="rn_PageContent123" class="rn_Home">
-    <div id="rn_PageTitle" class="rn_Home">
-        <h1>Service Status - Network Outage</h1>
-    </div>
-    <form id="target" action="">
-        <input id="searchParam" type="text" value="Hello there" />
-        <input id="submitBtn" type="submit" value="Go" />
-        <input id="queryType" type="hidden" value="Suburb" />
-    </form>
-
-    <div id="dynamic"></div>
-</div>
--->
-
 <div id="rn_PageContent123" class="rn_Home">
             <!-- Service Status pages start here! -->
 
