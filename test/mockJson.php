d1c8ae85d95db1472d7438194a850cb9da802635
Austin Xie <Austin.Xie@optus.com.au>
2013-07-25 01:57:05 +1000

    get ready for release.

--- test/mockJson.php
+++ test/mockJson.php
@@ -4,12 +4,12 @@
 
     $testJsonData = json_decode('{' .
     '"Unexpected":['.
-        '{"id":15,"serviceId":"2233","state":"NSW","location":"Epping","serviceAffected":"roaming","fixingStatus":"Fixed","outageType":"Internet, Phone & Digital Fetch TV services are experiencing service affecting issues","description":"N/A","type":"Planned", "startTime":"Monday 1st of July 2013 02:52:00 PM","endTime":"Saturday 3rd of August 2013 02:52:00 PM"},'.
+        '{"id":15,"serviceId":"2233","state":"NSW","location":"Epping","serviceAffected":null,"fixingStatus":"Fixed","outageType":"Internet, Phone & Digital Fetch TV services are experiencing service affecting issues","description":"N/A","type":"Planned", "startTime":"Monday 1st of July 2013 02:52:00 PM","endTime":"Saturday 3rd of August 2013 02:52:00 PM"},'.
         '{"id":25,"serviceId":"2244","state":"NSW","location":"Carlingford","serviceAffected":"Cable","fixingStatus":"Fixed","outageType":"Internet, Phone & Digital Fetch TV services are experiencing service affecting issues","description":"N/A","type":"Planned", "startTime":"Monday 1st of July 2013 02:52:00 PM","endTime":"Saturday 3rd of August 2013 02:52:00 PM"}'.
     '],' .
     '"Planned":[' .
-        '{"id":10,"serviceId":"1233","state":"NSW","location":"Epping","serviceAffected":"mobile","outageType":"Internet, Phone & Digital Fetch TV services are experiencing service affecting issues","description":"N/A","type":"Planned","startTime":"Monday 1st of July 2013 02:52:00 PM","endTime":"Saturday 3rd of August 2013 02:52:00 PM"},' .
-        '{"id":20,"serviceId":"1244","state":"NSW","location":"Carlingford","serviceAffected":"Cable","outageType":"Internet, Phone & Digital Fetch TV services are experiencing service affecting issues","description":"N/A","type":"Planned","startTime":"Monday 1st of July 2013 02:52:00 PM","endTime":"Saturday 3rd of August 2013 02:52:00 PM"}' .
+        '{"id":10,"serviceId":"1233","state":"NSW","location":"Epping","serviceAffected":"mobile","outageType":"Internet, Phone & Digital Fetch TV services are experiencing service affecting issues","description":null,"type":"Planned","startTime":"Monday 1st of July 2013 02:52:00 PM","endTime":"Saturday 3rd of August 2013 02:52:00 PM"},' .
+        '{"id":20,"serviceId":"1244","state":"NSW","location":"Carlingford","serviceAffected":"Cable","outageType":"Internet, Phone & Digital Fetch TV services are experiencing service affecting issues","description":null,"type":"Planned","startTime":"Monday 1st of July 2013 02:52:00 PM","endTime":"Saturday 3rd of August 2013 02:52:00 PM"}' .
     ']}');
 
     // echo json_encode($testJsonData->Planned);
