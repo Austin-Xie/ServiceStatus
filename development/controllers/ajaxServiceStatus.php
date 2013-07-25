061a227355d5baa25694d8990defa7deb28b6897
Austin Xie <Austin.Xie@optus.com.au>
2013-07-25 10:53:58 +1000

    - roll back to the implementation before applying 20-records limitation

--- development/controllers/ajaxServiceStatus.php
+++ development/controllers/ajaxServiceStatus.php
@@ -2,7 +2,6 @@
 
 class ajaxServiceStatus extends ControllerBase
 {
-	
     //This is the constructor for the custom controller. Do not modify anything within
     //this function.
     function __construct()
@@ -10,14 +9,7 @@ class ajaxServiceStatus extends ControllerBase
         parent::__construct();
 
         $this->load->model('custom/ServiceStatus/NetworkOutage_model');
-
     }
-	
-	function ajaxQueryTest() {
-		//echo '{"ajax": "OK!"}';
-		 echo '{"msg": "FFFFFFFFFFFFFFFKKKKKKKKKKKKKKKKKK"}';
-	}
-    
     
     /**
      * This function can be called by sending
@@ -31,18 +23,16 @@ class ajaxServiceStatus extends ControllerBase
 		}
 
 		// only 'Online' accessible.
-        $status = 3;
+        $status = 'Online';
         $nos = $this->NetworkOutage_model->getBySuburb($suburb, $status);
 		$planned = array();
         $unexpected = array();
         while($no = $nos->next())
         {
-		    //echo json_encode($no);
             $expNO = $this->exportNetworkOutageBrief($no);
-            if ($expNO['type'] === "Planned") {
+            if ($expNO['type'] === 'Planned') {
                 $planned[] = $expNO;
             } else {
-			     $expNO['type'] = "Unplanned";
                 $unexpected[] = $expNO;
             }
         }
@@ -85,29 +75,24 @@ class ajaxServiceStatus extends ControllerBase
     }
 
     function exportNetworkOutageBrief($no) {
-	    $NOType = array("1" => "Unplanned", "2" => "Planned");
-	    $NOState = array("1" => "ACT", "2" => "NSW", "3" => "VIC", "4" => "QLD",
-		                 "5" => "SA", "6" => "WA", "7" => "TAS", "8" => "NT");
-	    $NOFixStatus = array("1" => "Fixing", "2" => "Fixed");
-
         $exptNO = array();
 
-        $exptNO['id'] = $no['ID'];
-        $exptNO['serviceId'] = $no['OptusNetworkRef'];
+        $exptNO['id'] = $no->ID;
+        $exptNO['serviceId'] = $no->OptusNetworkRef;
         
-        $exptNO['state'] = $NOState[$no['State']];
-        $exptNO['location'] = $no['Areas'];
+        $exptNO['state'] = $no->State->Name;
+        $exptNO['location'] = $no->Areas;
         
-        $exptNO['serviceAffected'] = $no['CustomService'];
-        $exptNO['outageType'] = $no['CustomSummary'];
+        $exptNO['serviceAffected'] = $no->CustomService;
+        $exptNO['outageType'] = $no->CustomSummary;
 
-        $exptNO['type'] = $NOType[$no['Type']];
-       
-        $exptNO['startTime'] = date('g:ia D, jS M', $no['StartTime']);
-        $exptNO['endTime'] = date('g:ia D, jS M', $no['EndTime']);
-        
-		$exptNO['fixingStatus'] = $NOFixStatus[$no['FixingStatus']];
-        
+        $exptNO['type'] = $no->Type->Name;
+        if ($exptNO['type'] === "Planned") {
+            $exptNO['startTime'] = date('g:ia D, jS M', $no->StartTime);
+            $exptNO['endTime'] = date('g:ia D, jS M', $no->EndTime);
+        } else {
+            $exptNO['fixingStatus'] = $no->FixingStatus->Name;
+        }
 
         return $exptNO;
     }
@@ -120,11 +105,15 @@ class ajaxServiceStatus extends ControllerBase
             $exptNO[$key] = $value;
         }
 
-        $exptNO['updatedTime'] = date('Y-m-d H:i:s T', $no['UpdatedTime']);
+        if ($exptNO['type'] === "Unplanned") {
+            $exptNO['startTime'] = date('g:ia D, jS M', $no->StartTime);
+            $exptNO['endTime'] = date('g:ia D, jS M', $no->EndTime);
+        }
+        $exptNO['updatedTime'] = date('g:ia D, jS M', $no->UpdatedTime);
 
-        $exptNO['description'] = $no['CustomDescription'];
+        $exptNO['description'] = $no->CustomDescription;
         //$exptNO['technicalSummary'] = $no->TechnicalSummary;
-        $exptNO['resolution'] = $no['Resolution'];
+        $exptNO['resolution'] = $no->Resolution;
 
         return $exptNO;
     }
--- development/models/custom/ServiceStatus/networkoutage_model.php
+++ development/models/custom/ServiceStatus/networkoutage_model.php
@@ -11,10 +11,6 @@ class NetworkOutage_model extends Model
         initConnectAPI();
     }
 
-	function getNTs() {
-		$nt = RNCPHP\ROQL::query("SELECT nt.Name, nt.ID FROM CO.NwkOtgType nt ")->next();
-		return $nt;
-	}
    /**
     * Returns CO.NetworkOutage objects from the database based on their status and suburb.
     *
@@ -24,16 +20,12 @@ class NetworkOutage_model extends Model
     * @return Answer The CO.NetworkOutage with the specified answer_id
     */
     function getBySuburb($suburb, $status){
-        $rowLimit = 20;
-	    $serviceStatusQry = "SELECT no.ID, no.OptusNetworkRef, no.State, no.Areas, no.CustomService," .
-		                    " no.CustomSummary, no.Type, no.StartTime, no.EndTime, no.FixingStatus" .
-                            " FROM CO.NetworkOutage no WHERE ProcessStatus = 3"; //'"
-		                    //. $status . "' ";
+	    $serviceStatusQry = "SELECT CO.NetworkOutage FROM CO.NetworkOutage WHERE ProcessStatus.Name = '"
+		                    . $status . "' ";
         if (empty($suburb)) {
-            $nos = RNCPHP\ROQL::query($serviceStatusQry . " LIMIT " . $rowLimit)->next();
+            $nos = RNCPHP\ROQL::queryObject($serviceStatusQry)->next();
         } else {
-		    $nos = RNCPHP\ROQL::query($serviceStatusQry . " AND Areas like '%" . $suburb . "%'"
-			                          . " LIMIT " . $rowLimit)->next();
+		    $nos = RNCPHP\ROQL::queryObject($serviceStatusQry . " AND Areas like '%" . $suburb . "%'")->next();
         }
 
         return $nos;
@@ -47,17 +39,8 @@ class NetworkOutage_model extends Model
     * @return CO.NetworkOutage  The CO.NetworkOutage object with its details
     */
     function getNetworkOutageById($serviceStatusId) {
-	    $rowLimit = 20;
-	    $serviceStatusQry = "SELECT no.ID, no.OptusNetworkRef, no.State, no.Areas, no.CustomService," .
-		                    " no.CustomSummary, no.Type, no.StartTime, no.EndTime, no.FixingStatus" .
-							" no.UpdatedTime, no.CustomDescription, no.Resolution " .
-                            " FROM CO.NetworkOutage no WHERE  ID = ". $serviceStatusId .
-							" LIMMIT " . $rowLimit .
-							" ORDER BY no.StartTime DESC";
-							
-        //$no = RNCPHP\ROQL::queryObject("SELECT CO.NetworkOutage FROM CO.NetworkOutage WHERE  ID = " 
-        //      . $serviceStatusId)->next();
-		$no = RNCPHP\ROQL::query(serviceStatusQry)->next();
+        $no = RNCPHP\ROQL::queryObject("SELECT CO.NetworkOutage FROM CO.NetworkOutage WHERE  ID = " 
+              . $serviceStatusId)->next();
 
         return $no;
     }
