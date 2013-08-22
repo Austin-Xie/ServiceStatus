428c1813c0721c10f42c9218885166e627475ba3
Austin Xie <Austin.Xie@optus.com.au>
2013-08-22 17:26:22 +1000

    - Add Network Outage Mobile Forward Page

---
+++ development/views/pages/mobile/service/status/list.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<header>
+    <script type="text/javascript">/*<![CDATA[*/
+        var url = "http://optus.custhelp.com/app/service/status/list",
+            exdate = new Date(),
+            strExDate = exdate.setDate(exdate.getDate() + 7).toUCString();
+
+        document.cookie='agent=/;path=/;expires=' + strExDate;
+
+        $(location).attr('href', url);
+
+        /*]]>*/</script>
+</header>
+<body>
+</body>
+
+</html>
