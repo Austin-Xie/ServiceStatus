903da39cd6496b44eef1baacae4c45080025d2d7
Austin Xie <Austin.Xie@optus.com.au>
2013-08-22 17:42:10 +1000

    - compact codes

--- development/views/pages/mobile/service/status/list.php
+++ development/views/pages/mobile/service/status/list.php
@@ -5,14 +5,10 @@
         var url = "http://optus.custhelp.com/app/service/status/list",
             exdate = new Date(),
             strExDate = exdate.setDate(exdate.getDate() + 7).toUCString();
-
         document.cookie='agent=/;path=/;expires=' + strExDate;
-
         $(location).attr('href', url);
-
         /*]]>*/</script>
 </header>
 <body>
 </body>
-
 </html>
