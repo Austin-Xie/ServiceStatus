cd7536095884b0e1f62f8f7a41978212fcace3bd
Austin Xie <Austin.Xie@optus.com.au>
2013-08-22 18:37:07 +1000

    - refactored code

--- development/views/pages/mobile/service/status/list.php
+++ development/views/pages/mobile/service/status/list.php
@@ -2,11 +2,10 @@
 <html>
 <head>
     <script type="text/javascript">/*<![CDATA[*/
-        var url = "http://optus.custhelp.com/app/service/status/list",
-            exdate = new Date(),
-            strExDate = exdate.setDate(exdate.getDate() + 7).toUCString();
-        document.cookie='agent=/;path=/;expires=' + strExDate;
-        $(location).attr('href', url);
+        var exdate = new Date();
+        exdate.setDate(exdate.getDate() + 7);
+        document.cookie='agent=/;path=/;expires=' + exdate.toUTCString();
+        window.location = 'http://optus.custhelp.com/app/service/status/list';
         /*]]>*/</script>
 </head>
 <body>
