155240b88ede766fbd9bdde8adf60fda0b62fc4f
Austin Xie <Austin.Xie@optus.com.au>
2013-08-23 14:34:41 +1000

    - set cookie for /app/service/status/list to make forward work

--- development/views/pages/mobile/service/status/list.php
+++ development/views/pages/mobile/service/status/list.php
@@ -2,9 +2,10 @@
 <html>
 <head>
     <script type="text/javascript">/*<![CDATA[*/
-        var exdate = new Date();
-        exdate.setDate(exdate.getDate() + 7);
-        document.cookie='agent=/;path=/;expires=' + exdate.toUTCString();
+        var curCookie = document.cookie,
+            exdate = new Date();
+        exdate.setTime(exdate.getTime() + 600000);
+        document.cookie='agent=/;path=/app/service/status/list;expires=' + exdate.toUTCString();
         window.location = 'http://optus.custhelp.com/app/service/status/list';
         /*]]>*/</script>
 </head>
